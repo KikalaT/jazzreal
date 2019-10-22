f004e78db8ccbf2f6c35f22afec60c4ff0df33e7
jericho <jericho@jazzmusiclab.org>
2019-10-22 11:15:29 +0000

    change:static -> jazzreal/static

--- __init__.py
+++ __init__.py
@@ -5,10 +5,6 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
 SECRET_KEY='dev',
 )
-<<<<<<< HEAD
-# ensure the instance folder exists
-=======
->>>>>>> 119765ce96705c5203d8148c1fd3b091f4ac5c5f
 try:
 	os.makedirs(app.instance_path)
 except OSError:
